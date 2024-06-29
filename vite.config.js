c78e0fbb3df9ed55b2403f8543e38c0fd8045084
swizz <emailswizz@gmail.com>
2024-06-29 16:37:00 -0400

    ok

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  plugins: [react()],
   build: {
-    outDir: 'dist', // Ensure the output directory is set to 'dist'
     rollupOptions: {
-      input: './src/index.tsx'
-    }
-  }
+      input: './src/index.tsx',
+    },
+  },
 });
