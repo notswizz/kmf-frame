dc2a440f734ef2c8f89da253d41fe1630a0de334
swizz <emailswizz@gmail.com>
2024-06-29 16:30:45 -0400

    ok

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
-export default {
-    build: {
-      rollupOptions: {
-        input: './src/index.tsx'
-      }
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    outDir: 'dist', // Ensure the output directory is set to 'dist'
+    rollupOptions: {
+      input: './src/index.tsx'
     }
   }
-  
+});
